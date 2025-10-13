a6ae128346ba35e0677bcbb2713077451d996596
Runor Ewhro <rewhro@icloud.com>
2025-10-13 14:08:08 -0300

    added echo presets

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,6 @@ import './styles';
 // @ts-ignore
 import App from './App.jsx';
 
-console.log(import.meta.env);
-console.log('🔍 Build env check:', process.env.VITE_GOOGLE_CLIENT_ID);
 createRoot(document.getElementById('root')).render(
     <StrictMode>
         <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
