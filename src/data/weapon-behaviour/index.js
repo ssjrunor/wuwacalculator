b80080fd73e8f11b99ea7e41de9e571e92005f93
Runor Ewhro <rewhro@icloud.com>
2025-11-12 19:25:48 -0400

    updated echoGenerator

--- src/data/weapon-behaviour/index.js
+++ src/data/weapon-behaviour/index.js
@@ -1,18 +1,5 @@
 const modules = import.meta.glob('./*.js', { eager: true });
 
-const weaponLogicMap = {};
-
-for (const path in modules) {
-    const file = modules[path];
-    const weaponId = path.match(/\/(\d+)\.js$/)?.[1];
-
-    if (weaponId) {
-        if (file.applyWeaponLogic) {
-            weaponLogicMap[weaponId] = file.applyWeaponLogic;
-        }
-    }
-}
-
 export function getWeaponOverride(weaponId) {
     const logicModule = modules[`./${weaponId}.js`];
     if (!logicModule) return null;
--- src/data/weapon-ui/index.js
+++ src/data/weapon-ui/index.js
@@ -1,20 +1,11 @@
 const modules = import.meta.glob('./*.jsx', { eager: true });
 
 const weaponUIMap = {};
-const weaponLogicMap = {};
 
 for (const path in modules) {
     const file = modules[path];
     const weaponId = path.match(/\/(\d+)\.jsx$/)?.[1];
-
-    if (weaponId) {
-        if (file.WeaponUI) {
-            weaponUIMap[weaponId] = file.WeaponUI;
-        }
-        if (file.applyWeaponLogic) {
-            weaponLogicMap[weaponId] = file.applyWeaponLogic;
-        }
-    }
+    if (weaponId && file.WeaponUI) weaponUIMap[weaponId] = file.WeaponUI;
 }
 
 export function getWeaponUIComponent(weaponId) {
